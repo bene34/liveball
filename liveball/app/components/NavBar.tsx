2759d1b764227e3a4a90dc12802210304ba34e7d
Benjamin <benepstein4@gmail.com>
2026-05-26 05:30:57 -0700

    pls work

--- liveball/app/components/NavBar.tsx
+++ liveball/app/components/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <div style={{ width: "100%", borderBottom: "1px solid #f3f4f6", background: "#fff" }}>
-      <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "2 rem", display: "flex", alignItems: "center", justifyContent: "space-between", height: "52px" }}>
+      <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "2rem", display: "flex", alignItems: "center", justifyContent: "space-between", height: "52px" }}>
         <Link href="/" style={{ fontSize: "15px", fontWeight: 500, color: "#111827", fontFamily: "system-ui, sans-serif", textDecoration: "none", whiteSpace: "nowrap", margin: 0, maxWidth: "100%" }}>
           Hoops Measure
         </Link>
